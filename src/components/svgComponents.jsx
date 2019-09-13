9be1f1e5978638900cba639ae04adc935e4ae880
grom194 <brskoshelev@gmail.com>
2019-09-13 14:44:59 +0300

    add images

---
+++ src/components/svgComponents.jsx
@@ -0,0 +1,17 @@
+import CrossSign from '../../public/img/cross.svg';
+import CircleSign from '../../public/img/circle.svg';
+import GithubIcon from '../../public/img/github.svg';
+import TelegramIcon from '../../public/img/telegram.svg';
+import GmailIcon from '../../public/img/gmail.svg';
+import FacebookIcon from '../../public/img/facebook.svg';
+import StorybookIcon from '../../public/img/storybook.svg';
+
+export {
+  CrossSign,
+  CircleSign,
+  GithubIcon,
+  TelegramIcon,
+  GmailIcon,
+  FacebookIcon,
+  StorybookIcon,
+};
