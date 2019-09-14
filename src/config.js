a01129788f18069e9aa18dccbaa5745d7950b563
grom194 <brskoshelev@gmail.com>
2019-09-14 23:10:33 +0300

    add socket url variable for dev/prod

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const socketUrl = {
+  development: 'http://localhost:1337',
+  production: 'https://cms.bkoshelev.dev',
+};
+
+export { socketUrl };
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
+import { socketUrl } from '../config';
 
-const socket = io('http://localhost:1337');
+const socket = io(socketUrl[process.env.NODE_ENV]);
 
 export default socket;
